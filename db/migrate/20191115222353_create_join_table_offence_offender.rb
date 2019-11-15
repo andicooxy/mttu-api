4b52c57ba7d692f8e8f472d1ed7d763653b1a9af
Andy Awuku Oteng <andy.awuku@nfortics.com>
2019-11-15 23:00:20 +0000

    [fix] on migrations

--- db/migrate/20191115222353_create_join_table_offence_offender.rb
+++ db/migrate/20191115222353_create_join_table_offence_offender.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableOffenceOffender < ActiveRecord::Migration[5.2]
   def change
-    remove_colmn :offenders, :offenses_ids
+    remove_colomn :offenders, :offenses_ids
     create_join_table :offenders, :offenses do |t|
       t.index [:offender_id, :offense_id]
       t.index [:offense_id, :offender_id]
