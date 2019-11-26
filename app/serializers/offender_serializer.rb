e313ba1d4f058fe59787962e2dc64c2d2e282289
andycooxy <andycooxy@gmail.com>
2019-11-26 10:03:54 +0000

    [added] id to  offenders list

--- app/serializers/offender_serializer.rb
+++ app/serializers/offender_serializer.rb
@@ -6,12 +6,14 @@ module OffenderSerializer
         {full_name: full_name,
          statement: statement,
          created_by: user.cerialize_for_login,
-         created_at: created_at
+         created_at: created_at,
+         :id 
         }
     end
 
     def cerialize
       slice(:full_name,
+            :id,
             :statement,
             :created_at,
             :title)
