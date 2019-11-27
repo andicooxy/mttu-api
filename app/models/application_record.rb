fc12ceb3dd3d7d628343ed91feea5005e7cadd7c
andycooxy <andycooxy@gmail.com>
2019-11-27 09:25:04 +0000

    [added] filters to model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  scope :created_between, lambda {|start_date, end_date| where("created_at >= ? AND created_at <= ?", start_date.beginning_of_day, end_date.end_of_day)}
 end
--- app/models/offense.rb
+++ app/models/offense.rb
@@ -2,7 +2,6 @@ class Offense < ApplicationRecord
     include OffenseSerializer
     belongs_to :user
     has_and_belongs_to_many :offenders
-    scope :created_between, lambda {|start_date, end_date| where("created_at >= ? AND created_at <= ?", start_date.beginning_of_day, end_date.end_of_day)}
 
 end
 
